cbf297608bcd247374c0135a744679dd95d99885
Matthieu GUILLERMIN <matthieu.guillermin@m6.fr>
2014-12-19 11:47:54 +0100

    Dispatch Events on Elasticsearch requests
    The event name is "m6web.elasticsearch"

---
+++ src/M6Web/Bundle/ElasticsearchBundle/EventDispatcher/ElasticsearchEvent.php
@@ -0,0 +1,115 @@
+<?php
+
+
+namespace M6Web\Bundle\ElasticsearchBundle\EventDispatcher;
+
+
+use Symfony\Component\EventDispatcher\Event;
+
+/**
+ * Class ElasticsearchEvent
+ *
+ */
+class ElasticsearchEvent extends Event
+{
+    /**
+     * @var float duration of the Elasticsearch request in milliseconds
+     */
+    private $duration;
+
+    /**
+     * @var string HTTP method
+     */
+    private $method;
+
+    /**
+     * @var string Elasticsearch URI
+     */
+    private $uri;
+
+    /**
+     * @var int HTTP status code
+     */
+    private $statusCode;
+
+    /**
+     * @return float
+     */
+    public function getDuration()
+    {
+        return $this->duration;
+    }
+
+    /**
+     * @param float $duration
+     *
+     * @return $this
+     */
+    public function setDuration($duration)
+    {
+        $this->duration = $duration;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMethod()
+    {
+        return $this->method;
+    }
+
+    /**
+     * @param string $method
+     *
+     * @return $this
+     */
+    public function setMethod($method)
+    {
+        $this->method = $method;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUri()
+    {
+        return $this->uri;
+    }
+
+    /**
+     * @param string $uri
+     *
+     * @return $this
+     */
+    public function setUri($uri)
+    {
+        $this->uri = $uri;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getStatusCode()
+    {
+        return $this->statusCode;
+    }
+
+    /**
+     * @param int $statusCode
+     *
+     * @return $this
+     */
+    public function setStatusCode($statusCode)
+    {
+        $this->statusCode = $statusCode;
+
+        return $this;
+    }
+
+}
