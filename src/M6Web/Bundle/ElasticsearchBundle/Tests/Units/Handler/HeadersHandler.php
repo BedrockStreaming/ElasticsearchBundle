f3a173b7deb4ef64bf8cc1fb02c33e7164591c9b
Benoit Viguier <benoit.viguier@bedrockstreaming.com>
2021-06-28 16:44:44 +0200

    feat(header): Allow to add specific Host header

--- src/M6Web/Bundle/ElasticsearchBundle/Tests/Units/Handler/HeadersHandler.php
+++ src/M6Web/Bundle/ElasticsearchBundle/Tests/Units/Handler/HeadersHandler.php
@@ -55,25 +55,26 @@ class HeadersHandler extends atoum
     {
         return [
             [
-                'request'         => [],
-                'expectedRequest' => [],
+                'request'         => ['headers' => ['host' => ['localhost:9200']]],
+                'expectedRequest' => ['headers' => ['host' => ['localhost:9200']], 'url' => 'http://localhost:9200'],
                 'headers'         => [],
             ],
             [
                 'request'         => ['headers' => ['host' => ['localhost:9200']]],
-                'expectedRequest' => ['headers' => ['host' => ['localhost:9200']]],
-                'headers'         => [],
-            ],
-            [
-                'request'         => ['headers' => ['host' => ['localhost:9200']]],
-                'expectedRequest' => ['headers' => ['host' => ['localhost:9200'], 'X-AddMe' => ['Hello']]],
+                'expectedRequest' => ['headers' => ['host' => ['localhost:9200'], 'X-AddMe' => ['Hello']], 'url' => 'http://localhost:9200'],
                 'headers'         => ['X-AddMe' => ['Hello']],
+            ],
+            [
+                'request'         => ['headers' => ['host' => ['localhost:9200']]],
+                'expectedRequest' => ['headers' => ['host' => ['other-host.com']], 'url' => 'http://localhost:9200'],
+                'headers'         => ['host' => ['other-host.com']],
             ],
             [
                 'request'         => ['headers' => ['host' => ['localhost:9200']]],
                 'expectedRequest' => [
                     'headers' => ['host' => ['localhost:9200'], 'Accept-Encoding' => ['gzip']],
                     'client'  => ['decode_content' => true],
+                    'url' => 'http://localhost:9200'
                 ],
                 'headers'         => ['Accept-Encoding' => ['gzip']],
             ],
@@ -81,6 +82,7 @@ class HeadersHandler extends atoum
                 'request'         => ['headers' => ['host' => ['localhost:9200']]],
                 'expectedRequest' => [
                     'headers' => ['host' => ['localhost:9200'], 'Accept-Encoding' => ['deflate']],
+                    'url' => 'http://localhost:9200'
                 ],
                 'headers'         => ['Accept-Encoding' => ['deflate']],
             ],
